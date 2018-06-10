eb93fd2b88df3c1ee548269d08bc36bccafc157f
Chris Tsongas <chris.tsongas@gmail.com>
2018-06-10 17:25:27 +0000

    fix syntax errors

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -7,11 +7,11 @@ const {BlogPosts} = require('./models');
 // posts we initially add below
 function lorem() {
   return 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod ' +
-    'tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, '
+    'tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, ' +
     'quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo ' +
     'consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse ' +
     'cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non ' +
-    'proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    'proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 }
 
 // seed some posts so initial GET requests will return something
