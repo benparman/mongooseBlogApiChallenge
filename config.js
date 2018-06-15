5aa54dc5bdb5c68aa58ad254cfe01a649f0edd00
Ben Parman <benparman@gmail.com>
2018-06-15 17:12:54 -0600

    updated config.js to reflect new database name

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/mongoose-blog-api';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-mongoose-blog-api';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/mongoose-blog-api-challenge';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-mongoose-blog-api-challenge';
 exports.PORT = process.env.PORT || 8080;
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
