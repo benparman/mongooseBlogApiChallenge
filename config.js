e2027b7805b795b37051b3a30ebfe2f6056b889d
Ben Parman <benparman@gmail.com>
2018-06-15 17:21:11 -0600

    updated config.js again to reflect correct DB URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/mongoose-blog-api-challenge';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-mongoose-blog-api-challenge';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://ben:password1@ds261430.mlab.com:61430/mongoose-blog-api-challenge';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://ben:password1@ds261430.mlab.com:61430/mongoose-blog-api-challenge';
 exports.PORT = process.env.PORT || 8080;
